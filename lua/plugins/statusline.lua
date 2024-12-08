d4c835c9e8397fc5b5c4454afe4fd90e2fb6be12
Joram Liebeskind <joram@liebeskind.org>
2024-12-08 23:44:40 +0100

    Add statusline (lualine.nvim)

---
+++ lua/plugins/statusline.lua
@@ -0,0 +1,26 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        options = {
+            icons_enabled = true,
+            theme = 'auto',
+        },
+        sections = {
+            lualine_a = {"mode"},
+            lualine_b = {"FugitiveHead", "diff", "diagnostics"},
+            lualine_c = {"filename"},
+            lualine_x = {"encoding", "fileformat", {"filetype", icon = { align = 'right' }} },
+            lualine_z = {"location"},
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {"filename"},
+            lualine_x = {"location"},
+            lualine_y = {},
+            lualine_z = {},
+        },
+        extensions = { "neo-tree", "lazy", "fugitive" },
+    }
+}
