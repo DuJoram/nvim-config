a05377fcfbcd50bdc69fef177a047495d65b2a18
Joram Liebeskind <joram@liebeskind.org>
2024-12-09 01:20:16 +0100

    Add none-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -70,5 +70,28 @@ return {
                 lspconfig[server].setup(settings)
             end
         end
-    }
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = function()
+            local null_ls = require("null-ls")
+            return {
+                sources = {
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.fish_indent,
+                    null_ls.builtins.formatting.isort,
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.shfmt.with({ extra_args = { "-i", 2 }}),
+                    null_ls.builtins.diagnostics.fish,
+                },
+            }
+        end,
+        config = function(_, opts)
+            require("null-ls").setup(opts)
+        end
+
+    },
 }
