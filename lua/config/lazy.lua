2db0098fa0157636ae2a36485c93ef6d62964069
Joram Liebeskind <joram@liebeskind.org>
2024-12-10 13:14:31 +0100

    Add mechanism for locally adding which-key groups

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,3 +23,16 @@ require("lazy").setup({
 	},
 	checker = { enable = true },
 })
+
+-- This "extends" lazy to allow adding the 'wk_groups' key to plugin configurations.
+-- If present, they will be passed to which-key. This is useful for setting groups
+-- without needing the plugin to be loaded. Otherwise, some entries in the which-key
+-- menu will be without names until the plugin is first loaded. While this could be
+-- configured somewhere locally, I prefer each plugin to set up its groups locally.
+local plugins = require("lazy").plugins()
+for _, plugin in pairs(plugins) do
+	local wk = require("which-key")
+	if plugin["wk_groups"] then
+		wk.add(plugin.wk_groups)
+	end
+end
